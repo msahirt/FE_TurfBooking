91862c9e7ce84616802bdf919daf1368298eb92a
mohamed sahir <msahirt@gmail.com>
2024-06-19 08:47:07 +0530

    login and signup

---
+++ src/Components/Cards/Cards.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Cards.css'
+
+function Cards() {
+  return (
+    <div className='card'>
+        <img src="https://w7.pngwing.com/pngs/809/234/png-transparent-let-s-play-child-cbeebies-learning-postcard-game-text-people-thumbnail.png" alt="" />
+        <div className='card_content'>
+            <h2>
+                Court Name
+            </h2>
+            <p>
+                location <br />
+                lets play the game
+            </p>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Cards
--- src/Components/CourtListBody/CourtListBody.jsx
+++ src/Components/CourtListBody/CourtListBody.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Cards from '../Cards/Cards'
 
 function CourtListBody() {
   return (
     <div className='court_list_body'>
-        
+        <Cards/>
       
     </div>
   )
